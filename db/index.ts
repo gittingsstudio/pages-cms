5dedb325b5a0978693925ec352389855a33b2b18
Ryan Gittings <ryan@gittings.studio>
2025-03-04 21:48:37 +0000

    Fixes

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,7 @@
 import "@/db/envConfig";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from './schema';
-import { createClient } from "@libsql/client/web";
+import { createClient } from "@libsql/client";
 
 const client = createClient({
   url: process.env.SQLITE_URL!,
