cb62168909ba023eda3755e6723e41fc91a49539
Ryan Gittings <ryan@gittings.studio>
2025-06-09 12:43:42 +0100

    Add iframe

--- fields/core/rich-text/index.tsx
+++ fields/core/rich-text/index.tsx
@@ -59,6 +59,11 @@ const write = (value: any, field: Field, config: Record<string, any>) => {
       replacement: (content: string, node: any, options: any) => node.outerHTML
     });
 
+    turndownService.addRule("preserve-iframes", {
+      filter: "iframe",
+      replacement: (content: string, node: any) => node.outerHTML
+    });
+
     // We need to strip <colgroup> and <col> tags otherwise turndown won't convert tables
     content = content.replace(/<colgroup>.*?<\/colgroup>/g, '');
 
@@ -70,7 +75,7 @@ const write = (value: any, field: Field, config: Record<string, any>) => {
 
 const schema = (field: Field, configObject?: Record<string, any>) => {
   let zodSchema = z.string();
-  
+
   if (field.required) zodSchema = zodSchema.min(1, "This field is required");
   if (field.pattern) {
     if (typeof field.pattern === "string") {
@@ -81,7 +86,7 @@ const schema = (field: Field, configObject?: Record<string, any>) => {
   }
   if (field.options?.minlength) zodSchema = zodSchema.min(field.options.minlength as number, `Minimum length is ${field.options.minlength} characters`);
   if (field.options?.maxlength) zodSchema = zodSchema.max(field.options.maxlength as number, `Maximum length is ${field.options.maxlength} characters`);
-  
+
   return zodSchema;
 };
 
